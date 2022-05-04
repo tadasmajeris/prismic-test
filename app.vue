761d9c617ff0b8ad879219f85217579f382202cc
Tadas <tadas.majeris@gmail.com>
2022-05-04 12:49:26 +0100

    displaying cms data in frontend

--- app.vue
+++ app.vue
@@ -1,11 +1,19 @@
-<template>
-  <div>
-    <h1>hello</h1>
-    <div>{{home.data}}</div>
-  </div>
-</template>
-
 <script lang='ts' setup>
   const { client } = usePrismic()
   const { data: home } = await useAsyncData('home', () => client.getByUID('post', 'home'))
+  const cmsData = home.value.data
 </script>
+
+<template>
+  <div>
+    <h1>{{cmsData.title}}</h1>
+    <div>{{cmsData.content[0].text}}</div>
+    <img :src="cmsData.heroimage.url" />
+  </div>
+</template>
+
+<style scoped>
+  img {
+    max-width: 600px;
+  }
+</style>

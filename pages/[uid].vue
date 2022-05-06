9a5f89575633262c3bf174664e4c91e82628cb3e
Tadas <tadas.majeris@gmail.com>
2022-05-06 11:13:17 +0100

    page code updated

--- pages/[uid].vue
+++ pages/[uid].vue
@@ -1,18 +1,19 @@
 <script lang="ts" setup>
   import { useRoute } from 'vue-router';
   const route = useRoute();  
+  const uid = String(route.params.uid);
 
   const { client } = usePrismic()
-  const { data: cmsData } = await useAsyncData(route.params.uid, () => client.getByUID('post', route.params.uid))
-  const page = cmsData && cmsData.value ? cmsData.value.data : 'page not found'
+  const { data: cmsData } = await useAsyncData(uid, () => client.getByUID('post', uid))
+  const page = cmsData && cmsData.value ? cmsData.value.data : {title: 'page not found'}
 </script>
 
 <template>
   <main>
     <h1>{{page.title}}</h1>
-    <div>{{page.content[0].text}}</div>
-    <img :src="page.heroimage.url" />
-    <slice-zone v-if="page.slices && page.slices.length" :slices="page.slices" :components="components"/>
+    <div v-if="page.content">{{page.content[0].text}}</div>
+    <img v-if="page.heroimage" :src="page.heroimage.url" />
+    <slice-zone v-if="page.slices && page.slices.length" :slices="page.slices" />
   </main>
 </template>
 
